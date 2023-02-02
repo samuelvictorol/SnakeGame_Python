f5dfe2cbd20dfd13fc674e61336bd3f4b2ca61dd
Samuel Victor Oliveira Lima <990905>
2023-02-02 16:38:31 -0300

    add movimentos pra snake

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from turtle import Screen
 from snake import Snake
 import time
 
-player = str(input("Nome do jogador: "))
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title(f"Snake Game | {player}")
+screen.title("Snake Game")
 screen.tracer(0)
 
-snake = Snake(player)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.4J)
     snake.move()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,15 +1,18 @@
 from turtle import Turtle
-
+STARTING_POSITION = [(0,0), (-20, 0), (-40,0)]
+MOVE_DISTANCE = 20
 class Snake:
-    name = 'sem nome'
-    starting_position = [(0,0), (-20, 0), (-40,0)]
-    segments = []
+    
+    def __init__(self):
+        self.segments = []
+        self.generateSnake()
+        self.head = self.segments[0]
+        self.head.color("blue")
 
-    def __init__(self, name):
-        self.name = name
-        for position in self.starting_position:
+    def generateSnake(self):
+        for position in STARTING_POSITION:
             segment = Turtle("square")
-            segment.color("green")
+            segment.color("white")
             segment.penup() # tira rastro 
             segment.goto(position)
             self.segments.append(segment)
@@ -19,5 +22,17 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y) 
-        self.segments[0].forward(20)
-        self.segments[0].left(90)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+    
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
+
